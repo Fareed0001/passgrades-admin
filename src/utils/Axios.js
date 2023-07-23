9926e4ca145c460e74d637c17199d6e5d4a4b7b1
Farid <fareedibrahim0003@gmail.com>
2023-07-23 15:17:28 +0100

    https

--- src/utils/Axios.js
+++ src/utils/Axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://passmark.eu-north-1.elasticbeanstalk.com/api/v1/admin",
+  baseURL: "http://passmark.eu-north-1.elasticbeanstalk.com/api/v1/admin",
 });
