9cccf8226f2dac5f775a8615c9076fafc3c0a722
Farid <fareedibrahim0003@gmail.com>
2023-07-28 09:01:40 +0100

    card images and details updated

--- src/pages/Admin/AdminVerifyAgent/index.jsx
+++ src/pages/Admin/AdminVerifyAgent/index.jsx
@@ -10,34 +10,8 @@ const index = () => {
         <div class="agent-verification-div">
           {/* <!-- NEW AGENT CARD START  --> */}
           <div class="row row-cols-1 row-cols-md-2  row-cols-lg-3 g-4">
-            <div class="col agent-col">
-              <div class="new-agent-card">
-                <Image src="" class="card-img-top courses-card-img" alt="sat" />
-                <div class="agent-card-body">
-                  <p class="agent-card-title">Temitope David</p>
-                  <div class="row">
-                    <div class="col-6 d-grid">
-                      <button
-                        type="button"
-                        class="btn agent-card-button btn-outline-primary"
-                      >
-                        Accept
-                      </button>
-                    </div>
-                    <div class="col-6 d-grid">
-                      <button
-                        type="button"
-                        class="btn agent-card-button btn-outline-danger"
-                      >
-                        Reject
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
 
-            <div class="col">
+            {/* <div class="col agent-col">
               <div class="new-agent-card">
                 <Image src="" class="card-img-top courses-card-img" alt="sat" />
                 <div class="agent-card-body">
@@ -62,34 +36,8 @@ const index = () => {
                   </div>
                 </div>
               </div>
-            </div>
+            </div> */}
 
-            <div class="col">
-              <div class="new-agent-card">
-                <Image src="" class="card-img-top courses-card-img" alt="sat" />
-                <div class="agent-card-body">
-                  <p class="agent-card-title">Temitope David</p>
-                  <div class="row">
-                    <div class="col-6 d-grid">
-                      <button
-                        type="button"
-                        class="btn agent-card-button btn-outline-primary"
-                      >
-                        Accept
-                      </button>
-                    </div>
-                    <div class="col-6 d-grid">
-                      <button
-                        type="button"
-                        class="btn agent-card-button btn-outline-danger"
-                      >
-                        Reject
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
           </div>
         </div>
       </div>
